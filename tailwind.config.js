205143856b8e3608dbd851ffc3c8008ce1d71dc0
Zakary Ben El Gharib <zakary34750@gmail.com>
2024-11-12 20:25:01 +0100

    FinalVersion

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./**/*.html', "./node_modules/flowbite/**/*.js"],
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat'],
+      },
+      spacing: {
+        // Ajoutez des espacements personnalisés si nécessaire
+      }
+    }
+  },
+
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
